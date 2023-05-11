6e663999bf9b40464eb42d0bfbeb053b153c403a
marian-mrn <marian.ciurdas@yahoo.com>
2023-05-11 10:40:16 +0300

    First project commit.

---
+++ Tasks/TaskViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskViewController.swift
+//  Tasks
+//
+//  Created by home on 10/05/2023.
+//
+
+import UIKit
+
+class TaskViewController: UIViewController {
+
+    @IBOutlet var label: UILabel!
+    
+    var task: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        label.text = task
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Delete", style: .done, target: self, action: #selector(deleteTask))
+    }
+    
+    @objc func deleteTask() {
+//
+//        let newCount = count - 1
+//
+//        UserDefaults().setValue(newCount, forKey: "count")
+//        UserDefaults().setValue(nil, forKey: "task_\(currentPosition)")
+       
+    }
+
+    
+
+}
